a098f17c023d27420ed2b1a1c62660267930637a
Deias <saied_am@hotmail.com>
2022-03-29 14:14:49 +0200

    bug unknown email crashes the app resolved

--- server.py
+++ server.py
@@ -26,8 +26,12 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    if [club for club in clubs if club['email'] == request.form['email']]:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+        return render_template('welcome.html',club=club,competitions=competitions)
+    else:
+        flash("Unknown email !", "error")
+        return render_template('index.html')
 
 
 @app.route('/book/<competition>/<club>')
