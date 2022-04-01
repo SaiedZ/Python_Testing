7f14dbe31de6e4e6416835c914d01409fdc0a142
Deias <saied_am@hotmail.com>
2022-04-01 19:05:39 +0200

    adding view and template to be able to see the list of clubs and their associated current points balance

--- server.py
+++ server.py
@@ -98,7 +98,9 @@ def purchasePlaces():
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
-# TODO: Add route for points display
+@app.route('/pointsBoard', methods=['GET'])
+def pointsBoard():
+    return render_template('points_board.html', clubs=clubs)
 
 
 @app.route('/logout')
