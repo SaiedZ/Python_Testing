db4b9d60163012ead6cdf7db1955497e94ab93e1
Deias <saied_am@hotmail.com>
2022-04-02 14:17:21 +0200

    adding performance test

---
+++ tests/performance/locustfile.py
@@ -0,0 +1,31 @@
+from locust import HttpUser, task
+
+
+class PerformanceTest(HttpUser):
+
+    @task
+    def home(self):
+        self.client.get('')
+
+    @task
+    def pointsBoard(self):
+        self.client.get('pointsBoard')
+
+    @task
+    def summary(self):
+        data = {"email": "kate@shelifts.co.uk"}
+        self.client.post("showSummary", data=data)
+
+    @task
+    def booking_page(self):
+        club = "She Lifts"
+        competition = "Fall Classic"
+        self.client.get(f'book/{competition}/{club}', )
+
+    @task
+    def purchasePlaces(self):
+        """Book test. Should not take more than 2 seconds. Default number of users: 6. """
+        club = "She Lifts"
+        competition = "Fall Classic"
+        data = {"competition": competition, "club": club, "places": 1}
+        self.client.post('purchasePlaces', data=data)
