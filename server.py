bfd6f7a60e944262f6029bb1bc092f39b3ff3812
Deias <saied_am@hotmail.com>
2022-04-05 17:13:47 +0200

    purchased points are now stored in a json files -purchases- and used in the view to define if the club can purchase more places or not

--- server.py
+++ server.py
@@ -30,18 +30,50 @@ def loadCompetitions():
          return listOfCompetitions
 
 
+def load_purchases():
+    with open("purchases.json") as purchases_file:
+        purchases_dict = json.load(purchases_file)
+        return purchases_dict
+
+
+def load_competition_places_purchased_by_club(club, competition):
+    club_email = club['email']
+    competition_name = competition['name']
+    purchases_dict = load_purchases()
+    try:
+        purchased_places = int(
+            purchases_dict[club_email][competition_name]
+        )
+    except KeyError:
+        purchased_places = 0
+
+    return purchased_places
+
+
 def update_json_data(file, data):
     with open(file, 'w') as file:
         json.dump(data, file, indent=4, separators=(',', ': '))
 
 
+def update_purchases(purchases_dict, club_email, competition_name, value):
+    if club_email in purchases_dict:
+        if competition_name in purchases_dict:
+            purchases_dict[club_email][competition_name] = value
+        else:
+            purchases_dict[club_email] = {competition_name: value}
+    else:
+        purchases_dict[club_email] = {competition_name: value}
+
+
 competitions = loadCompetitions()
 clubs = loadClubs()
+purchases_dict = load_purchases()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
@@ -75,36 +107,52 @@ def purchasePlaces():
 
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
+    club_index = clubs.index(club)
+    competition_index = competitions.index(competition)
+    places_required = int(request.form['places'])
+    places_already_bought = load_competition_places_purchased_by_club(club, competition)
+
+    problem_to_purchase = False
 
     if compare_str_date_to_now(competition['date']):
         flash("You can't book a place on a post-dated competition! ")
-        return render_template('booking.html', club=club, competition=competition)
+        problem_to_purchase = True
 
-    if placesRequired > config.MAX_BOOKABLE_PLACES:
-        flash(f"You are not allowed to purchase more than {config.MAX_BOOKABLE_PLACES} places ! ")
-        return render_template('booking.html', club=club, competition=competition)
+    if int(request.form['places']) <= 0:
+        flash("Places to book should be an integer > 0")
+        problem_to_purchase = True
+
+    if places_already_bought >= config.MAX_BOOKABLE_PLACES:
+        flash("You already booked the maximum of places for this competition! ")
+        problem_to_purchase = True
 
     max_places_to_book = math.floor(int(club['points']) / config.POINTS_PER_PLACE)
 
-    if placesRequired > max_places_to_book:
+    if places_required > max_places_to_book:
         flash("You don't have enough points ! ")
-        return render_template('booking.html', club=club, competition=competition)
+        problem_to_purchase = True
 
-    if placesRequired > int(competition['numberOfPlaces']):
+    if places_required > int(competition['numberOfPlaces']):
         flash("You can't book more places than available ! ")
+        problem_to_purchase = True
+
+    if (places_required + places_already_bought) > config.MAX_BOOKABLE_PLACES:
+        flash(f"You are not allowed to purchase more than {config.MAX_BOOKABLE_PLACES} places ! ")
+        flash(f"You already bougth {places_already_bought} for this competition! ")
+        problem_to_purchase = True
+
+    if problem_to_purchase:
         return render_template('booking.html', club=club, competition=competition)
 
-    club['points'] = int(club['points']) - (placesRequired * config.POINTS_PER_PLACE)
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+    club['points'] = int(club['points']) - (places_required * config.POINTS_PER_PLACE)
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
 
-    club_index = clubs.index(club)
-    competition_index = competitions.index(competition)
     clubs[club_index] = club
     competitions[competition_index] = competition
-
-    update_json_data('clubs.json', {'clubs': clubs})
-    update_json_data('competitions.json', {'competitions': competitions})
+    update_purchases(purchases_dict, club['email'], competition['name'], places_already_bought + places_required)
+    update_json_data("clubs.json", {'clubs': clubs})
+    update_json_data("competitions.json", {'competitions': competitions})
+    update_json_data("purchases.json", purchases_dict)
 
     flash('Great-booking complete!')
 
