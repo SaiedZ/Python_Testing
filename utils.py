7f0691bf68ce4dfe931204d96095eced034be189
Deias <saied_am@hotmail.com>
2022-04-11 11:38:02 +0200

    adding docstring to utils file

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+"""This module contains utility functions."""
+
 from flask import flash
 
 from datetime import datetime
@@ -8,6 +10,8 @@ from data import data_utils
 
 
 def compare_str_date_to_now(str_date):
+    """Return True if the date passed inf the params is
+    a past date, otherwise it return False."""
     datetime_object = datetime.strptime(str_date, "%Y-%m-%d %H:%M:%S")
     return datetime_object < datetime.now()
 
@@ -15,6 +19,9 @@ def compare_str_date_to_now(str_date):
 def update_purchases_data(competition, club, places_required,
                           places_already_bought, purchases_dict,
                           clubs, competitions):
+    """Updates the purchases and the json files of purchases and
+    clubs and competitions.
+    """
     data_utils.update_purchases(
         purchases_dict,
         club["email"],
@@ -34,20 +41,23 @@ def verrify_condition_are_satisfied_to_purchase_places(
 ):
     """
     Verrify if all conditions are satisfied to allow a club to purchase
-    places. If some condition are not satisfied, a message is flashed
+    places.
+    
+    If some condition are not satisfied, a message is flashed
     and a booleen value is returned as False.
-    If All conditions are satisfied True is returned.
+    
+    If all conditions are satisfied True is returned.
     """
     problem_to_purchase = True
-
+    # can't book postdated competition
     if compare_str_date_to_now(competition["date"]):
         flash("You can't book a place on a post-dated competition! ")
         problem_to_purchase = False
-
+    # négati places numbers are not accepted
     if int(request.form["places"]) <= 0:
         flash("Places to book should be an integer > 0")
         problem_to_purchase = False
-
+    # can't book if already booked the maximum allowed number
     if places_already_bought >= config.MAX_BOOKABLE_PLACES:
         flash(
             "You already booked the maximum of places for this competition! ")
@@ -55,15 +65,15 @@ def verrify_condition_are_satisfied_to_purchase_places(
 
     max_places_to_book = \
         math.floor(int(club["points"]) / config.POINTS_PER_PLACE)
-
+    # can't book more than the maximum allowed
     if places_required > max_places_to_book:
         flash("You don't have enough points ! ")
         problem_to_purchase = False
-
+    # can't book more than available places
     if places_required > int(competition["numberOfPlaces"]):
         flash("You can't book more places than available ! ")
         problem_to_purchase = False
-
+    # can't book if the sum of already booked places and places to book is > max
     if (places_required + places_already_bought) > config.MAX_BOOKABLE_PLACES:
         flash(
             f"You are not allowed to purchase more than {config.MAX_BOOKABLE_PLACES} places ! "
